306175f3544fcbc398e52e70191946221374e00a
Daniel <danizmd8@gmail.com>
2023-11-07 10:11:32 +0300

    [Feature] Added formulas and formula logging

--- examples/example_1.py
+++ examples/example_1.py
@@ -8,6 +8,18 @@ def main():
     C_TIME = 17
     PLOT_PATH = "plot.jpg"
 
+    """
+    probabilities
+        [0.34, 0.64, 0.02]
+        [0.83, 0.14, 0.03]
+        [0.56, 0.44]
+
+    times
+        [5, 4, 1]
+        [9, 8, 6]
+        [3, 2]
+    """
+
     tps = TimeProfileSolver(
         [
             [0.51, 0.43, 0.06],
@@ -22,25 +34,37 @@ def main():
     print("Все ряды:")
     pprint(tps.results)
 
+    print("Все формулы:")
+    pprint([[x.formula for x in row] for row in tps.results])
+
     tps.sort_last_row()
-    print("Последний ряд после сортировки")
+    print("Последний ряд после сортировки:")
     pprint(tps.get_last_row())
+
+    print("Формулы последнего ряда:")
+    pprint([x.formula for x in tps.get_last_row()])
 
     tps.collect_last_row()
     print("Последний ряд после сборки")
     pprint(tps.get_last_row())
 
-    e = tps.get_math_expectation()
-    print("Мат. ожидание")
+    e, e_log = tps.get_math_expectation_logged()
+    print("Мат. ожидание:")
     pprint(e)
+    print("Вычисление мат. ожидания:")
+    print(e_log)
 
-    d = tps.get_variance()
-    print("Дисперсия")
+    d, d_log = tps.get_variance_logged()
+    print("Дисперсия:")
     pprint(d)
+    print("Вычисление дисперсии:")
+    print(d_log)
 
-    r = tps.get_risk(C_TIME)
+    r, r_log = tps.get_risk_logged(C_TIME)
     print("Риск")
     pprint(r)
+    print("Вычисление риска:")
+    print(r_log)
 
     ImageDrawer(*RequestWayMapper(tps.get_last_row()).map(), PLOT_PATH).draw()
 
--- time_profiler_soa/time_profile_solver.py
+++ time_profiler_soa/time_profile_solver.py
@@ -4,12 +4,26 @@ from typing import Tuple
 
 from time_profiler_soa.tps_types import Numeric
 
+# TODO: refactor round(x, ROUND_NDIGITS)
+ROUND_NDIGITS = 4
+
 
 @dataclass
 class RequestWay:
     name: str
     probability: Numeric
     time: Numeric
+    formula_fmt: str = "U{}(K{})"
+    formula: str = ""
+
+    def get_rounded_probability(self) -> Numeric:
+        return round(self.probability, ROUND_NDIGITS)
+
+    def get_formated_formula_name(self) -> str:
+        return self.formula_fmt.format(self.name, self.time)
+
+    def __str__(self) -> str:
+        return f"{self.formula_fmt.format(self.name, self.time)}={round(self.time, ROUND_NDIGITS)}"
 
 
 class RequestWayMapper:
@@ -32,11 +46,9 @@ class TimeProfileSolver:
         self,
         probabilities: list[list[Numeric]],
         times: list[list[Numeric]],
-        name_literal: str = "W",
     ):
         self.probabilities = probabilities
         self.times = times
-        self.name_literal = name_literal
         self.results: list[list[RequestWay]] = [[]]
 
         self._sorted = False
@@ -48,9 +60,12 @@ class TimeProfileSolver:
 
         # first step - just append first service info to results
         for i, (p, t) in enumerate(zip(self.probabilities[0], self.times[0])):
-            rw = RequestWay(f"{self.name_literal}{i + 1}", p, t)
+            rw = RequestWay(f"{i + 1}", p, t)
+            rw.formula = rw.get_formated_formula_name()
+
             self.results[0].append(rw)
 
+        # for every other service compute probability and time
         for service_id in range(1, len(self.probabilities)):
             self.results.append([])
 
@@ -65,6 +80,29 @@ class TimeProfileSolver:
                     )
 
                     self.results[-1].append(rw)
+                    rw.formula = str(
+                        str(rw)
+                        + " = "
+                        + " * ".join(
+                            [
+                                str(last_rw),
+                                str(
+                                    RequestWay(
+                                        str(service_id + 1), service_p, service_t
+                                    )
+                                ),
+                            ]
+                        )
+                        + " = "
+                        + " * ".join(
+                            [
+                                str(round(p, ROUND_NDIGITS)),
+                                str(round(service_p, ROUND_NDIGITS)),
+                            ]
+                        )
+                        + " = "
+                        + str(rw.get_rounded_probability())
+                    )
 
     def sort_last_row(self):
         if self._sorted:
@@ -102,6 +140,22 @@ class TimeProfileSolver:
 
         return reduce(lambda s, v: s + (v.time * v.probability), self.get_last_row(), 0)
 
+    def get_math_expectation_logged(self) -> Tuple[Numeric, str]:
+        expectation = self.get_math_expectation()
+
+        log = (
+            "E = "
+            + " + ".join(
+                [
+                    f"{round(x.time, ROUND_NDIGITS)} * {round(x.probability, ROUND_NDIGITS)}"
+                    for x in self.get_last_row()
+                ]
+            )
+            + f" =  {round(expectation, ROUND_NDIGITS)}"
+        )
+
+        return expectation, log
+
     def get_variance(self) -> Numeric:
         expectation = self.get_math_expectation()
 
@@ -111,7 +165,24 @@ class TimeProfileSolver:
             0,
         )
 
-    def get_risk(self, C_time):
+    def get_variance_logged(self) -> Tuple[Numeric, str]:
+        expectation = self.get_math_expectation()
+        variance = self.get_variance()
+
+        log = (
+            "D = "
+            + " + ".join(
+                [
+                    f"({round(x.time, ROUND_NDIGITS)}-{round(expectation, ROUND_NDIGITS)})^2 * {round(x.probability, ROUND_NDIGITS)}"
+                    for x in self.get_last_row()
+                ]
+            )
+            + f" = {round(variance, ROUND_NDIGITS)}"
+        )
+
+        return variance, log
+
+    def get_risk(self, C_time) -> Numeric:
         if not self._sorted:
             self.sort_last_row()
 
@@ -123,6 +194,26 @@ class TimeProfileSolver:
             self.get_last_row(),
             0,
         )
+
+    def get_risk_logged(self, C_time) -> Tuple[Numeric, str]:
+        risk = self.get_risk(C_time)
+
+        last_row = self.get_last_row()
+
+        last_index = 0
+        for index, rw in enumerate(last_row):
+            if rw.time > C_time:
+                last_index = index
+
+        last_row = last_row[: last_index + 1]
+
+        log = (
+            "R = 1 - ("
+            + " + ".join([f"{round(x.probability, ROUND_NDIGITS)}" for x in last_row])
+            + f") = {round(risk, ROUND_NDIGITS)}"
+        )
+
+        return risk, log
 
     def get_last_row(self):
         return self.results[-1]
